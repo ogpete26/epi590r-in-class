7260a1608cdaa0a9bebe45a62f0fd5911a676a7b
Olivia Peterson <petersongolivia@gmail.com>
2024-08-20 16:04:13 -0400

    making functions

--- docs/making_function.R
+++ docs/making_function.R
@@ -1,8 +1,19 @@
-x <- c(1,3,5,7,9)
-
-new_mean <- function() {
-	n <- length(x)
-	mean_val <- sum(x) / n
-	return(mean_val)
+square <- function(x) {
+	sq_val <- x*x
+	return(sq_val)
 }
 
+y <- 52
+square(y)
+52^2
+
+
+prop <- function(x, multiplier=1) {
+	n <- length(x)
+	proportion_val <- sum(x)/n
+	multiplied_val <- multiplier*proportion_val
+	return(multiplied_val)
+}
+
+prop(c(1,1,1,0,0))
+prop(c(1,1,1,0,0), multiplier = 100)
